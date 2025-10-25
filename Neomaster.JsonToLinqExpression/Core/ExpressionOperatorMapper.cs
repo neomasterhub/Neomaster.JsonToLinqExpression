190f61d3fd226369b6f6204c220af6785f84329c
Neomaster X <neomasterx@outlook.com>
2025-10-26 00:47:41 +0300

    #5 Or and And are required in ExpressionOperatorMapper.

--- Neomaster.JsonToLinqExpression/Core/ExpressionOperatorMapper.cs
+++ Neomaster.JsonToLinqExpression/Core/ExpressionOperatorMapper.cs
@@ -1,8 +1,16 @@
+using System.Linq.Expressions;
+
 namespace Neomaster.JsonToLinqExpression;
 
 public class ExpressionOperatorMapper
 {
   public readonly Dictionary<string, Consts.ExpressionBind> Operators = [];
+
+  public ExpressionOperatorMapper(string or, string and)
+  {
+    Operators.Add(or, Expression.Or);
+    Operators.Add(and, Expression.And);
+  }
 
   public ExpressionOperatorMapper Add(string op, Consts.ExpressionBind bind)
   {
