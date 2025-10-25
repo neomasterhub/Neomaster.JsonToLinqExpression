ab66e11e05a716a768cebd2cdb26e8c72fdc7da9
Neomaster X <neomasterx@outlook.com>
2025-10-25 23:52:41 +0300

    #1 ExpressionOperatorMappers.Default

--- Neomaster.JsonToLinqExpression/Consts.cs
+++ Neomaster.JsonToLinqExpression/Consts.cs
@@ -5,4 +5,19 @@ namespace Neomaster.JsonToLinqExpression;
 public static class Consts
 {
   public delegate Expression ExpressionBind(Expression left, Expression right);
+
+  public static class ExpressionOperatorMappers
+  {
+    public static readonly ExpressionOperatorMapper Default = new ExpressionOperatorMapper()
+      .Add("or", Expression.Or)
+      .Add("and", Expression.And)
+      .Add("eq", Expression.Equal)
+      .Add("neq", Expression.NotEqual)
+      .Add("gt", Expression.GreaterThan)
+      .Add("gte", Expression.GreaterThanOrEqual)
+      .Add("lt", Expression.LessThan)
+      .Add("lte", Expression.LessThanOrEqual)
+      // TODO: contains, does not contain
+      ;
+  }
 }
