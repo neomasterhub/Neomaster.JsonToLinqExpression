f1c2c7a2d29e435bdd6c23f552dffdc18ce44b9e
Neomaster X <neomasterx@outlook.com>
2025-10-25 16:50:20 +0300

    ExpressionFieldMapper, Add()

---
+++ Neomaster.JsonToLinqExpression/Core/ExpressionFieldMapper.cs
@@ -0,0 +1,13 @@
+namespace Neomaster.JsonToLinqExpression;
+
+public class ExpressionFieldMapper
+{
+  public readonly Dictionary<string, ExpressionField> Fields = [];
+
+  public ExpressionFieldMapper Add(string srcFieldName, ExpressionField dstField)
+  {
+    Fields.Add(srcFieldName, dstField);
+
+    return this;
+  }
+}
