34f09863a2a9cafc11b62b37b04ac613303862d8
Neomaster X <neomasterx@outlook.com>
2025-10-25 17:15:40 +0300

    ExpressionRule.Parse()

--- Neomaster.JsonToLinqExpression/Core/ExpressionRule.cs
+++ Neomaster.JsonToLinqExpression/Core/ExpressionRule.cs
@@ -9,4 +9,24 @@ public class ExpressionRule
   public string Field { get; set; }
   public JsonElement? Value { get; set; }
   public ConstantExpression ValueConstantExpression { get; set; }
+
+  public static ExpressionRule Parse(
+    JsonElement jsonElement,
+    ExpressionFieldMapper mapper,
+    string fieldPropertyName = nameof(Field),
+    string valuePropertyName = nameof(Value))
+  {
+    var srcFieldName = jsonElement.GetProperty(fieldPropertyName).GetString();
+    var srcValue = jsonElement.GetProperty(valuePropertyName);
+    var field = mapper.Fields[srcFieldName];
+    var rule = new ExpressionRule
+    {
+      Operator = field.Operator,
+      Field = field.Name,
+      Value = srcValue,
+      ValueConstantExpression = field.GetValue(srcValue),
+    };
+
+    return rule;
+  }
 }
