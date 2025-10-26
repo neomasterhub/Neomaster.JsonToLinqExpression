f0d5cc304062c8de14b4dba1269f6708b63e759f
Neomaster X <neomasterx@outlook.com>
2025-10-26 16:32:20 +0300

    #8 CreateExpressionBindTest<TResult>()

--- Neomaster.JsonToLinqExpression.UnitTests/ExpressionTools/ExpressionHelperUnitTests.cs
+++ Neomaster.JsonToLinqExpression.UnitTests/ExpressionTools/ExpressionHelperUnitTests.cs
@@ -1,5 +1,30 @@
+using System.Linq.Expressions;
+using System.Text.Json;
+
 namespace Neomaster.JsonToLinqExpression.UnitTests;
 
 public class ExpressionHelperUnitTests
 {
+  public static void CreateExpressionBindTest<TResult>(
+    string logicOperator,
+    Consts.ExpressionBind logicOperatorExpression,
+    Func<Expression, LambdaExpression> buildLambda,
+    Expression leftExpression,
+    Expression rightExpression,
+    TResult expectedLambdaResult)
+  {
+    var condition = new { logic = logicOperator };
+    var logicOperatorPropertyName = nameof(condition.logic);
+    var operatorMapper = new ExpressionOperatorMapper().Add(logicOperator, logicOperatorExpression);
+    var conditionJsonElement = JsonSerializer.SerializeToElement(condition);
+    var bind = ExpressionHelper.CreateExpressionBind(
+      conditionJsonElement,
+      logicOperatorPropertyName,
+      operatorMapper);
+
+    var binded = bind(leftExpression, rightExpression);
+    var lambda = buildLambda(binded).Compile();
+
+    Assert.Equal(expectedLambdaResult, lambda.DynamicInvoke());
+  }
 }
