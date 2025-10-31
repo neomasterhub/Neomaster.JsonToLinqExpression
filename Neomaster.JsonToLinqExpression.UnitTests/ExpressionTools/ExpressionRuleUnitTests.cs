176a11f2a55f4bbd19a9099378250db275089851
Neomaster X <neomasterx@outlook.com>
2025-10-31 12:34:01 +0300

    #11 tests: Validating field

--- Neomaster.JsonToLinqExpression.UnitTests/ExpressionTools/ExpressionRuleUnitTests.cs
+++ Neomaster.JsonToLinqExpression.UnitTests/ExpressionTools/ExpressionRuleUnitTests.cs
@@ -1,6 +1,7 @@
 using System.Linq.Expressions;
 using System.Text.Json;
 using Xunit.Abstractions;
+using static Neomaster.JsonToLinqExpression.Consts;
 
 namespace Neomaster.JsonToLinqExpression.UnitTests;
 
@@ -41,5 +42,67 @@ public class ExpressionRuleUnitTests(ITestOutputHelper output)
     Assert.Equal(rule.value, ruleParsed.ValueConstantExpression.Value);
     Assert.Equal(expectedField, ruleParsed.Field);
     Assert.Equal(expectedValue, ruleParsed.Value);
+  }
+
+  [Fact]
+  public void Parse_ShouldThrowKeyNotFoundException_InvalidFieldPropertyName()
+  {
+    const string invalidKey = "0";
+    var rule = new
+    {
+      op = "1",
+      field = "2",
+      value = "3",
+    };
+    var ruleJson = JsonSerializer.Serialize(rule);
+    var ruleJsonElement = JsonSerializer.SerializeToElement(rule);
+    var expectedExMessage = string.Format(ErrorMessages.JsonPropertyNotFound, invalidKey);
+    var mapper = new ExpressionFieldMapper()
+      .Add(
+        rule.field,
+        new ExpressionField
+        {
+          Name = rule.field,
+          GetValue = je => Expression.Constant(je.Value.GetString()),
+        });
+
+    var ex = Assert.Throws<KeyNotFoundException>(() => ExpressionRule.Parse(
+      ruleJsonElement,
+      mapper,
+      nameof(rule.op),
+      invalidKey,
+      nameof(rule.value)));
+
+    Assert.Equal(expectedExMessage, ex.Message);
+    Assert.Equal(invalidKey, ex.Data[ErrorDataKeys.JsonPropertyNotFound.Property]);
+    Assert.Equal(ruleJson, ex.Data[ErrorDataKeys.JsonPropertyNotFound.Json]);
+  }
+
+  [Fact]
+  public void Parse_ShouldThrowInvalidOperationException_InvalidFieldPropertyType()
+  {
+    var rule = new
+    {
+      op = "1",
+      field = 2,
+      value = "3",
+    };
+    var fieldPropertyName = nameof(rule.field);
+    var ruleJson = JsonSerializer.Serialize(rule);
+    var ruleJsonElement = JsonSerializer.SerializeToElement(rule);
+    var expectedExMessage = string.Format(ErrorMessages.JsonPropertyNotType, fieldPropertyName, JsonValueKind.String);
+
+    var ex = Assert.Throws<InvalidOperationException>(() => ExpressionRule.Parse(
+      ruleJsonElement,
+      new(),
+      nameof(rule.op),
+      fieldPropertyName,
+      nameof(rule.value)));
+
+    Assert.Equal(expectedExMessage, ex.Message);
+    Assert.Equal(fieldPropertyName, ex.Data[ErrorDataKeys.JsonPropertyNotType.Property]);
+    Assert.Equal(ruleJson, ex.Data[ErrorDataKeys.JsonPropertyNotType.Json]);
+    Assert.Equal(JsonValueKind.String, ex.Data[ErrorDataKeys.JsonPropertyNotType.ExpectedType]);
+    Assert.Equal(JsonValueKind.Number, ex.Data[ErrorDataKeys.JsonPropertyNotType.CurrentType]);
   }
 }
