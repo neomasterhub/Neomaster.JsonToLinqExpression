a3f02f63e607462cecbd612be89bba17171df36f
Neomaster X <neomasterx@outlook.com>
2025-11-02 06:15:32 +0300

    #44 Add class JsonLinq.

---
+++ Neomaster.JsonToLinq/JsonLinq.cs
@@ -0,0 +1,8 @@
+namespace Neomaster.JsonToLinq;
+
+/// <summary>
+/// Provides methods to convert JSON-based query definitions into LINQ expressions.
+/// </summary>
+public static class JsonLinq
+{
+}
